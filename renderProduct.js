9c602eb76fe7160162fa2d7bfb6113d63912faea
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-11 16:14:22 -0700

    render prodcut js file

---
+++ renderProduct.js
@@ -0,0 +1,17 @@
+export function renderPlant(plant){
+    const plantList = document.createElement('div');
+    plantList.classList.add('plant-list');
+    
+    const plantHeader = document.createElement('h2');
+    plantHeader.textContent = plant.name;
+
+    let addButton = document.createElement('button');
+    addButton.textContent = 'Add To Cart';
+    addButton.setAttribute('id', plant.id);
+    
+    const img = document.createElement('img');
+    img.src = plant.img;
+    
+    plantList.append(plantHeader, img, addButton);
+    return plantList;
+}
